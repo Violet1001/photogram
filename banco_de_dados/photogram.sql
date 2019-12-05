84fc27a290261a13e9208c8406ad3b889fcdeabe
Iorkman <ojpzinho@gmail.com>
2019-12-05 12:02:11 -0200

    banco att

--- banco_de_dados/photogram.sql
+++ banco_de_dados/photogram.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.7.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: 04-Dez-2019 às 18:38
--- Versão do servidor: 5.7.24
--- versão do PHP: 7.3.1
+-- Host: localhost
+-- Generation Time: 05-Dez-2019 às 14:55
+-- Versão do servidor: 5.6.34
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,9 +28,8 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `cadastro`
 --
 
-DROP TABLE IF EXISTS `cadastro`;
-CREATE TABLE IF NOT EXISTS `cadastro` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cadastro` (
+  `id_usuario` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `sobrenome` varchar(200) NOT NULL,
   `data_nascimento` date NOT NULL,
@@ -39,12 +38,15 @@ CREATE TABLE IF NOT EXISTS `cadastro` (
   `usuario` varchar(200) NOT NULL,
   `sexo` enum('masculino','feminino','outro') NOT NULL,
   `voto` enum('sim','nao') NOT NULL,
-  `foto_perfil` varchar(200) NOT NULL,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE KEY `foto_perfil` (`foto_perfil`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `usuario` (`usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+  `foto_perfil` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `cadastro`
+--
+
+INSERT INTO `cadastro` (`id_usuario`, `nome`, `sobrenome`, `data_nascimento`, `email`, `senha`, `usuario`, `sexo`, `voto`, `foto_perfil`) VALUES
+(13, 'lka', 'po', '2019-12-20', 'k@l', '123', 'mkdsa', 'masculino', 'nao', 'mk-2019.12.05-14.04.02.png');
 
 -- --------------------------------------------------------
 
@@ -52,16 +54,67 @@ CREATE TABLE IF NOT EXISTS `cadastro` (
 -- Estrutura da tabela `imagem`
 --
 
-DROP TABLE IF EXISTS `imagem`;
-CREATE TABLE IF NOT EXISTS `imagem` (
+CREATE TABLE `imagem` (
   `nome_imagem` varchar(200) NOT NULL,
   `tipo` enum('paisagem','meme','pessoa','animal','comida','decoracao','outro') NOT NULL,
   `data` date NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  PRIMARY KEY (`nome_imagem`),
-  KEY `id_usuario` (`id_usuario`),
-  KEY `id_usuario_2` (`id_usuario`)
+  `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `imagem`
+--
+
+INSERT INTO `imagem` (`nome_imagem`, `tipo`, `data`, `id_usuario`) VALUES
+('adventuretime.jpg', 'outro', '2020-01-10', 13),
+('cachoeira.jpg', 'paisagem', '2019-12-16', 13),
+('flores.jpg', 'paisagem', '2019-12-02', 13),
+('halloween.jpg', 'decoracao', '2019-10-30', 13),
+('halloween2.jpg', 'decoracao', '2019-11-02', 13),
+('hamburger.jpg', 'comida', '2019-12-17', 13),
+('joao-2019.12.02-19.11.40.jpg', 'animal', '2019-11-01', 13),
+('joao-2019.12.02-19.36.46.jpg', 'animal', '2019-12-25', 13),
+('joao-2019.12.04-23.38.47.jpg', 'meme', '2019-12-02', 13),
+('mkdsa-2019.12.05-14.06.28.jpg', 'pessoa', '2019-12-05', 13),
+('natal.jpg', 'decoracao', '2019-12-23', 13),
+('natal2.jpg', 'decoracao', '2019-11-12', 13),
+('noitche.jpg', 'paisagem', '2019-12-08', 13),
+('paisagem.jpg', 'paisagem', '2019-12-03', 13),
+('pastel.jpg', 'comida', '2019-12-24', 13),
+('pizza.jpg', 'comida', '2019-12-30', 13),
+('ramen.jpg', 'comida', '2019-12-17', 13),
+('regularshow.jpg', 'outro', '2019-12-09', 13);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cadastro`
+--
+ALTER TABLE `cadastro`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `foto_perfil` (`foto_perfil`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- Indexes for table `imagem`
+--
+ALTER TABLE `imagem`
+  ADD PRIMARY KEY (`nome_imagem`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_usuario_2` (`id_usuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cadastro`
+--
+ALTER TABLE `cadastro`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
